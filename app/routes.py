d32b2ff3ac0c7d8729cef646c1cbffbcf177eb58
Vitaly Belenky <vitaly.belenky@gmail.com>
2020-06-20 00:05:55 +0300

    Chapter 10: Email Support

--- app/routes.py
+++ app/routes.py
@@ -9,11 +9,14 @@ from werkzeug.urls import url_parse
 
 from app import app
 from app import db
+from app.email import send_password_reset_email
 from app.forms import LoginForm, EditProfileForm, EmptyForm
-from app.forms import RegistrationForm
-from app.models import User, Post
 from app.forms import PostForm
+from app.forms import RegistrationForm
+from app.forms import ResetPasswordForm
+from app.forms import ResetPasswordRequestForm
 from app.models import Post
+from app.models import User
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -70,6 +73,37 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
+
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
 
 
 @app.route('/logout')
