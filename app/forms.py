7b5e4574c14ace9756b01a311a84ea37ad7a84de
Vitaly Belenky <vitaly.belenky@gmail.com>
2020-06-19 23:27:39 +0300

    Chapter 9: Pagination

--- app/forms.py
+++ app/forms.py
@@ -49,5 +49,11 @@ class EditProfileForm(FlaskForm):
                 raise ValidationError('Please use a different username.')
 
 
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
+
+
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
